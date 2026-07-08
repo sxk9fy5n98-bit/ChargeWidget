22f0f1b05a6d6033d3c7b117231882a54898f8be
Jose Ignacio Montivero <j.montivero@icloud.com>
2026-07-08 18:50:42 +1000

    Initial Commit

---
+++ ChargeWidget/ChargeWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChargeWidgetApp.swift
+//  ChargeWidget
+//
+//  Created by Jose Ignacio Montivero on 8/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct ChargeWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
